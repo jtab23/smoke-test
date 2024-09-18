c769b7d1f3cefde8d7868c6994528cd723aa62c3
jtab23 <joshtablair@gmail.com>
2024-09-18 15:16:08 +0100

    dont break

--- src/App.js
+++ src/App.js
@@ -247,7 +247,6 @@ function App() {
                     <button
                       key={item.name}
                       onClick={handleNavigationClick}
-                      href={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
